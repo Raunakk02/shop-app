6284aa1c7988602ea3019e38fa79d7e2ff3dd175
Raunakk02 <raunakk728@gmail.com>
2020-06-21 12:21:32 +0530

    added expandible product list to my orders screen

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,10 +1,19 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import '../providers/order.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   OrderItem(this.order);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +24,40 @@ class OrderItem extends StatelessWidget {
         child: Column(
           children: [
             ListTile(
-              title: Text('Total Amount: \$ ${order.totalAmount.toStringAsFixed(2)}'),
-              subtitle: Text('Placed On: ${order.dateTime}'),
+              title: Text(
+                  'Total Amount: \$ ${widget.order.totalAmount.toStringAsFixed(2)}'),
+              subtitle: Text('Placed On: ${widget.order.dateTime}'),
               trailing: IconButton(
-                icon: Icon(Icons.expand_more),
-                onPressed: () {},
+                icon: _isExpanded
+                    ? Icon(Icons.expand_less)
+                    : Icon(Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _isExpanded = !_isExpanded;
+                  });
+                },
               ),
             ),
+            if (_isExpanded)
+              Container(
+                width: double.infinity,
+                height: min(widget.order.cartItems.length * 20.0 + 10, 100),
+                child: ListView.builder(
+                  itemBuilder: (ctx,index){
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8,horizontal: 5),
+                      child: Row(
+                        children: [
+                          Text(widget.order.cartItems[index].title),
+                          Spacer(),
+                          Text('${widget.order.cartItems[index].quantity} X \$${widget.order.cartItems[index].price}')
+                        ],
+                      ),
+                    );
+                  },
+                  itemCount: widget.order.cartItems.length,
+                ),
+              ),
           ],
         ),
       ),
