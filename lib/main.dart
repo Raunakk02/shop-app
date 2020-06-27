ad576a07d368faf094ca65b974c804a3676bd517
Raunakk02 <raunakk728@gmail.com>
2020-06-27 17:24:02 +0530

    added auth screen and relevant error handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_practice/screens/edit_product_screen.dart';
 
+import './providers/auth.dart';
 import './providers/produts.dart';
 import './providers/cart.dart';
 import './providers/order.dart';
 
+import './screens/auth_screen.dart';
 import './screens/products_overview_screen.dart';
 import './screens/products_detail_screen.dart';
 import './screens/carts_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
+import './screens/edit_product_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,6 +23,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(
+          create: (_) => Auth(),
+        ),
         ChangeNotifierProvider(
           create: (_) => Products(),
         ),
@@ -38,11 +43,11 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.purple,
           accentColor: Colors.deepOrange,
         ),
-        home: ProductsOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-          CartsScreen.routeName: (ctx) => CartsScreen(),      
-          OrdersScreen.routeName: (ctx) => OrdersScreen(),    
+          CartsScreen.routeName: (ctx) => CartsScreen(),
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
           UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
           EditProductScreen.routeName: (ctx) => EditProductScreen(),
         },
---
+++ lib/providers/auth.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:shop_practice/models/http_exception.dart';
+
+class Auth with ChangeNotifier {
+  String _token;
+  String _userId;
+  DateTime _expiryDate;
+
+  Future<void> _authenticate(String email, String password,
+      [bool _isLogin = false]) async {
+    final urlSegment = _isLogin ? 'signInWithPassword' : 'signUp';
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDflkZ3m3rkTrb8Bt0Mdq_3G5I4NICa5A0';
+
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        }),
+      );
+
+      final responseData = json.decode(response.body);
+
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+
+      print(json.decode(response.body));
+    }catch (error) {
+      throw error;
+    }
+
+    notifyListeners();
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password);
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, true);
+  }
+}
