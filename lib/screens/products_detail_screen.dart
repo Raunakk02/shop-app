42ea8a1f8de02ef0c4ba4b0f18f37373d5c558f6
Raunakk02 <raunakk728@gmail.com>
2020-07-01 16:29:57 +0530

    added sliver Animation to product details

--- lib/screens/products_detail_screen.dart
+++ lib/screens/products_detail_screen.dart
@@ -10,57 +10,68 @@ class ProductDetailScreen extends StatelessWidget {
     final productId = ModalRoute.of(context).settings.arguments;
     final selectedProd = Provider.of<Products>(context).findById(productId);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(selectedProd.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Hero(
-                tag: selectedProd.id,
-                child: Image.network(
-                  selectedProd.imageUrl,
-                  fit: BoxFit.cover,
+      // appBar: AppBar(
+      //   title: Text(selectedProd.title),
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(selectedProd.title),
+              background: Container(
+                height: 300,
+                width: double.infinity,
+                child: Hero(
+                  tag: selectedProd.id,
+                  child: Image.network(
+                    selectedProd.imageUrl,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-              decoration: BoxDecoration(
-                border: Border.symmetric(
-                  vertical: BorderSide(
-                    color: Colors.grey,
-                    width: 8,
+                decoration: BoxDecoration(
+                  border: Border.symmetric(
+                    vertical: BorderSide(
+                      color: Colors.grey,
+                      width: 8,
+                    ),
                   ),
                 ),
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              '\$ ${selectedProd.price}',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(
+                height: 10,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              width: double.infinity,
-              alignment: Alignment.center,
-              child: Text(
-                selectedProd.description,
+              Text(
+                '\$ ${selectedProd.price}',
                 style: TextStyle(
-                  color: Colors.grey[900],
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
-                softWrap: true,
+                textAlign: TextAlign.center,
               ),
-            ),
-          ],
-        ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                child: Text(
+                  selectedProd.description,
+                  style: TextStyle(
+                    color: Colors.grey[900],
+                  ),
+                  softWrap: true,
+                ),
+              ),
+              SizedBox(height: 800,)
+            ]),
+          ),
+        ],
       ),
     );
   }
